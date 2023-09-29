5eb6e6f482d6d31bf8da43f25dfabcb88031927d
dharrunkrish <130247682+dharrunkrish@users.noreply.github.com>
2023-09-30 00:58:03 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,28 @@
+
+
+public class App {
+    public static void main(String[] args) {
+        int len=9;
+        for(int i=1;i<=len;i++){
+            for(int j=1;j<i;j++){
+            System.out.print(" ");
+        }
+    
+        for(int k=1;k<=i;k++){
+            System.out.print("* ");
+        }
+     
+            System.out.println();
+        
+
+            
+
+        }
+    }
+}
+
+
+       
+
+
+        
